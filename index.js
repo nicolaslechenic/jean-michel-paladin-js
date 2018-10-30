aacd2d316a0194195f533aef9cd1787e60c5c4cf
Nicolas Le Chenic <nicolaslechenic@gmail.com>
2018-10-30 09:20:30 +0100

    refacto: move index.js functions to game

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ import World from './src/World.js'
 const displayer = document.getElementById('displayer')
 const submit = document.getElementById('button')
 
-let turn = 0
 let world = new World()
 let hero = new Hero('Jean-Michel Paladin')
+let game = new Game(displayer)
 
 world.enemies = [
   new Enemy('Balrog'),
@@ -16,47 +16,6 @@ world.enemies = [
   new Enemy('Squelette')
 ]
 
-show()
+game.ask(hero, world)
 
-submit.addEventListener('click', (event) => { choose(event.target.value) })
-
-function show () {
-  if (turn === 0) {
-    displayer.innerHTML = `<br/><br/>Ainsi débutent les aventures de ${hero.name}<br/><br/>`
-  }
-
-  displayer.innerHTML += `<br/>☰☰☰☰☰☰☰☰☰☰☰☰ Tour numéro ${turn} ☰☰☰☰☰☰☰☰☰☰☰☰<br/><br/>`
-  Game.displayActions(world, displayer)
-  displayer.innerHTML += '<br/>QUELLE ACTION FAIRE ?'
-}
-
-function gameIsOver () { displayer.innerHTML = 'Game Over !' }
-
-function choose (value) {
-  let choice = Number(value)
-
-  if (choice === 0) {
-    hero.heal()
-  } else if (choice === 1) {
-    hero.improveBonus()
-  } else if (choice === 99) {
-    gameIsOver()
-  } else {
-    let target = world.enemies[choice - 2]
-    hero.hit(target)
-  }
-
-  displayer.innerHTML = ''
-  displayer.innerHTML += '<br/>LES ENNEMIS RIPOSTENT !'
-
-  world.currentEnemies().forEach((enemy) => { enemy.hit(hero) })
-
-  displayer.innerHTML += `<br/>Etat du héro: ${hero.info()}<br/>`
-
-  if (Game.isOver(hero, world)) {
-    gameIsOver()
-  } else {
-    turn++
-    show()
-  }
-}
+submit.addEventListener('click', (event) => { game.choose(hero, world, event.target.value) })
--- src/Game.js
+++ src/Game.js
@@ -1,20 +1,67 @@
 class Game {
+  constructor (displayer) {
+    this.displayer = displayer
+    this.turn = 0
+  }
+
+  increaseTurn () { this.turn += 1 }
+  displayOver () { this.displayer.innerHTML = 'Game Over !' }
+
   damage () {
     return Math.floor(Math.random() * Math.floor(15))
   }
 
-  static displayActions (world, displayer) {
-    displayer.innerHTML += 'ACTIONS POSSIBLES :<br/>'
-    displayer.innerHTML += ' 0 ●●●● Se soigner<br/>'
-    displayer.innerHTML += ' 1 ●●●● Améliorer son attaque<br/>'
+  ask (hero, world) {
+    if (this.turn === 0) {
+      this.displayer.innerHTML = `<br/><br/>Ainsi débutent les aventures de ${hero.name}<br/><br/>`
+    }
+
+    this.displayer.innerHTML += `<br/>☰☰☰☰☰☰☰☰☰☰☰☰ Tour numéro ${this.turn} ☰☰☰☰☰☰☰☰☰☰☰☰<br/><br/>`
+    this.displayActions(world)
+    this.displayer.innerHTML += '<br/>QUELLE ACTION FAIRE ?'
+  }
+
+  displayActions (world) {
+    this.displayer.innerHTML += 'ACTIONS POSSIBLES :<br/>'
+    this.displayer.innerHTML += ' 0 ●●●● Se soigner<br/>'
+    this.displayer.innerHTML += ' 1 ●●●● Améliorer son attaque<br/>'
 
     let i = 2
     world.enemies.forEach((enemy) => {
-      displayer.innerHTML += ` ${i} ●●●● Attaquer ${enemy.info()}<br/>`
+      this.displayer.innerHTML += ` ${i} ●●●● Attaquer ${enemy.info()}<br/>`
       i++
     })
 
-    displayer.innerHTML += ' 99 ●●●● Quitter le jeu<br/>'
+    this.displayer.innerHTML += ' 99 ●●●● Quitter le jeu<br/>'
+  }
+
+  choose (hero, world, value) {
+    let choice = Number(value)
+
+    if (choice === 0) {
+      hero.heal()
+    } else if (choice === 1) {
+      hero.improveBonus()
+    } else if (choice === 99) {
+      this.displayOver()
+    } else {
+      let target = world.enemies[choice - 2]
+      hero.hit(target)
+    }
+
+    this.displayer.innerHTML = ''
+    this.displayer.innerHTML += '<br/>LES ENNEMIS RIPOSTENT !'
+
+    this.world.currentEnemies().forEach((enemy) => { enemy.hit(hero) })
+
+    this.displayer.innerHTML += `<br/>Etat du héro: ${this.hero.info()}<br/>`
+
+    if (Game.isOver(hero, world)) {
+      this.displayOver()
+    } else {
+      this.increaseTurn()
+      this.ask(hero, world)
+    }
   }
 
   static isOver (hero, world) {
